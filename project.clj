024a69c08cdc7594652542a576202aa42190bdd7
Stefan Fehrenbach <stefan.fehrenbach@gmail.com>
2014-03-17 15:44:14 +0100

    Add Truffle dependency.

--- project.clj
+++ project.clj
@@ -1,9 +1,11 @@
 (defproject tuber-basic "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :description "A BASIC on Truffle"
+  :url "https://github.com/fehrenbach/tuber-basic"
+  :license {:name "MIT"
+            :url "http://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [com.oracle/truffle "0.1"]]
+  :repositories {"truffle" "http://lafo.ssw.uni-linz.ac.at/nexus/content/repositories/releases/"}
   :main ^:skip-aot tuber-basic.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
