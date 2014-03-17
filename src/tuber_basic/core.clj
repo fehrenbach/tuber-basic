f395456fc8c854e96eabe277b88f5d79b2e644d8
Stefan Fehrenbach <stefan.fehrenbach@gmail.com>
2014-03-17 16:59:37 +0100

    I actually miss SDF.

--- src/tuber_basic/core.clj
+++ src/tuber_basic/core.clj
@@ -1,7 +1,16 @@
 (ns tuber-basic.core
+  (:require [clojure.java.io]
+            [instaparse.core :as insta])
   (:import [com.oracle.truffle.api Truffle]))
 
 (println (.getName (Truffle/getRuntime)))
+
+(def parser (insta/parser (clojure.java.io/resource "tuber-basic.bnf")))
+
+(parser "PRINT  \"57\"
+PRINT 5
+PRINT abc
+")
 
 (defn -main
   "I don't do a whole lot ... yet."
